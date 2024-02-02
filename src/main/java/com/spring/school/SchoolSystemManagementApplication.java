ead90a06bebd629470bb4a854220b339dffad457
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2024-02-01 21:25:54 -0300

    first commit

---
+++ src/main/java/com/spring/school/SchoolSystemManagementApplication.java
@@ -0,0 +1,13 @@
+package com.spring.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSystemManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSystemManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/school/SchoolSystemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSystemManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
