f5bf77cbf1f8c4fc8e07b6bf7712dcb552133d9e
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2024-02-01 21:57:26 -0300

    fix: allow CORS

--- src/main/java/com/spring/school/controller/StudentController.java
+++ src/main/java/com/spring/school/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,30 +25,35 @@ public class StudentController {
 	private StudentServices services;
 	
 	@GetMapping("/fullsearch")
+	@CrossOrigin(allowedHeaders = "*", origins = "*")
 	public ResponseEntity<List<StudentEntity>> findAll() {
 		List<StudentEntity> list = services.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping("/findById/{id}")
+	@CrossOrigin(allowedHeaders = "*", origins = "*")
 	public ResponseEntity<StudentEntity> findById(@PathVariable Long id) {
 		StudentEntity obj = services.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
 	@PostMapping("/save")
+	@CrossOrigin(allowedHeaders = "*", origins = "*")
 	public ResponseEntity<StudentEntity> insert(@RequestBody StudentEntity obj) {
 		services.insert(obj);
 		return ResponseEntity.ok().build();
 	}
 	
 	@DeleteMapping("/delete/{id}")
+	@CrossOrigin(allowedHeaders = "*", origins = "*")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		services.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@PutMapping("/update/{id}")
+	@CrossOrigin(allowedHeaders = "*", origins = "*")
 	public ResponseEntity<StudentEntity> update(@PathVariable Long id, @RequestBody StudentEntity obj) {
 		services.update(id, obj);
 		return ResponseEntity.ok().build();
